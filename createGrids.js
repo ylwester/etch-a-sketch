814d3b802995094349161441de14741ed33d351c
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2020-11-13 01:06:24 +0100

    Add slider to give Grid Size and creating new Grid button.

--- createGrids.js
+++ createGrids.js
@@ -1,4 +1,16 @@
 const container = document.querySelector(".grid-container");
+const old_html = $(".grid-container").html();
+
+let output = document.getElementById('value-output')
+let slider = document.getElementById('sizeRange');
+let newGridValue = output.innerHTML = slider.value;
+newGridValue = slider.oninput =  function () { output.innerHTML = this.value};
+
+function getGridValue () {
+    return document.getElementById('sizeRange').value;
+}
+
+
 
 function createRows(row, column){
     container.style.setProperty('--grid-rows', row);
@@ -6,9 +18,16 @@ function createRows(row, column){
     for(let i = 0; i < (row * column); i++){
         const cell = document.createElement('div');
         cell.classList.add('grid-item');
-        cell.innerText="aa";
         container.appendChild(cell);
+        cell.addEventListener('mouseover', e => {
+            cell.style.backgroundColor = 'black';
+        })
+        cell.setAttribute('style', 'border: 1px solid black')
     }
+}
+
+function updateDiv() {
+    $(".grid-container").html(old_html);
 }
 
 createRows(16, 16);
